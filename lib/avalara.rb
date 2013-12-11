86bc53ad283ad7f2e1196452d0dfd02aea7e8e1e
Matt Smith <matt@nearapogee.com>
2013-12-11 08:04:52 -0800

    move class methods into module.

--- lib/avalara.rb
+++ lib/avalara.rb
@@ -11,99 +11,103 @@ require 'avalara/request'
 require 'avalara/response'
 
 module Avalara
-
-  def self.configuration
-    @@_configuration ||= Avalara::Configuration.new
-    yield @@_configuration if block_given?
-    @@_configuration
-  end
-
-  def self.configuration=(configuration)
-    raise ArgumentError, 'Expected a Avalara::Configuration instance' unless configuration.kind_of?(Configuration)
-    @@_configuration = configuration
-  end
-
-  def self.configure(&block)
-    configuration(&block)
-  end
-  
-  def self.endpoint
-    configuration.endpoint
-  end
-  def self.endpoint=(endpoint)
-    configuration.endpoint = endpoint
-  end
-
-  def self.username
-    configuration.username
-  end
-  def self.username=(username)
-    configuration.username = username
-  end
-
-  def self.password
-    configuration.password
-  end
-  def self.password=(password)
-    configuration.password = password
-  end
-
-  def self.version
-    configuration.version
-  end
-  def self.version=(version)
-    configuration.version = version
-  end
-  
-  def self.geographical_tax(latitude, longitude, sales_amount)
-    uri = [
-      configuration.endpoint, 
-      configuration.version, 
-      "tax", 
-      "#{latitude},#{longitude}",
-      "get"
-    ].join("/")
-    
-    response = API.get(uri, 
-      :headers    => API.headers_for('0'),
-      :query      => {:saleamount => sales_amount},
-      :basic_auth => authentication
-    )
-
-    Avalara::Response::Tax.new(response)
-  rescue Timeout::Error
-    puts "Timed out"
-    raise TimeoutError
-  end
-    
-  def self.get_tax(invoice)
-    uri = [endpoint, version, 'tax', 'get'].join('/')
-
-    response = API.post(uri, 
-      :body => invoice.to_json,
-      :headers => API.headers_for(invoice.to_json.length),
-      :basic_auth => authentication
-    )
-
-    return case response.code
-      when 200..299
-        Response::Invoice.new(response)
-      when 400..599
-        raise ApiError.new(Response::Invoice.new(response))
-      else
-        raise ApiError.new(response)
+  module ClassMethods
+    def configuration
+      @@_configuration ||= Avalara::Configuration.new
+      yield @@_configuration if block_given?
+      @@_configuration
     end
-  rescue Timeout::Error => e
-    raise TimeoutError.new(e)
-  rescue ApiError => e
-    raise e
-  rescue Exception => e
-    raise Error.new(e)
-  end
-  
-  private
 
-  def self.authentication
-    { :username => username, :password => password}
+    def configuration=(configuration)
+      raise ArgumentError, 'Expected a Avalara::Configuration instance' unless configuration.kind_of?(Configuration)
+      @@_configuration = configuration
+    end
+
+    def configure(&block)
+      configuration(&block)
+    end
+
+    def endpoint
+      configuration.endpoint
+    end
+    def endpoint=(endpoint)
+      configuration.endpoint = endpoint
+    end
+
+    def username
+      configuration.username
+    end
+    def username=(username)
+      configuration.username = username
+    end
+
+    def password
+      configuration.password
+    end
+    def password=(password)
+      configuration.password = password
+    end
+
+    def version
+      configuration.version
+    end
+    def version=(version)
+      configuration.version = version
+    end
+
+    def geographical_tax(latitude, longitude, sales_amount)
+      uri = [
+        configuration.endpoint, 
+        configuration.version, 
+        "tax", 
+        "#{latitude},#{longitude}",
+        "get"
+      ].join("/")
+
+      response = API.get(uri, 
+        :headers    => API.headers_for('0'),
+        :query      => {:saleamount => sales_amount},
+        :basic_auth => authentication
+      )
+
+      Avalara::Response::Tax.new(response)
+    rescue Timeout::Error
+      puts "Timed out"
+      raise TimeoutError
+    end
+
+    def get_tax(invoice)
+      uri = [endpoint, version, 'tax', 'get'].join('/')
+
+      response = API.post(uri,
+        :body => invoice.to_json,
+        :headers => API.headers_for(invoice.to_json.length),
+        :basic_auth => authentication
+      )
+
+      return case response.code
+        when 200..299
+          Response::Invoice.new(response)
+        when 400..599
+          raise ApiError.new(Response::Invoice.new(response))
+        else
+          raise ApiError.new(response)
+      end
+    rescue Timeout::Error => e
+      raise TimeoutError.new(e)
+    rescue ApiError => e
+      raise e
+    rescue Exception => e
+      raise Error.new(e)
+    end
+
+    private
+
+    def authentication
+      { :username => username, :password => password}
+    end
   end
+
+  extend ClassMethods
+
 end
